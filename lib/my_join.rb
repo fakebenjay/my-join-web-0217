4cf53d8ddcaf8f9d29a2b22495517417aa377f13
fakebenjay <fakebenjay@gmail.com>
2017-06-18 12:16:31 -0400

    Done.

--- lib/my_join.rb
+++ lib/my_join.rb
@@ -1,1 +1,14 @@
-# your code goes here
+class Array
+  def my_join(string="")
+    text = ""
+
+    self.each do |a|
+      if self.index(a) == 0
+        text = a.to_s
+      else
+        text << "#{string}#{a.to_s}"
+      end
+    end
+    return text
+  end
+end
